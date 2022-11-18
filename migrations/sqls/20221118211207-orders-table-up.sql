f9bf1d5cef7ed2c31aeca0f732c7cbd012de338e
abdulaziz <aabuainain@nic.gov.sa>
2022-11-19 00:56:43 +0300

    fix bug on user db migartion foreign key

--- migrations/sqls/20221118211207-orders-table-up.sql
+++ migrations/sqls/20221118211207-orders-table-up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    status VARCHAR(20),
-    user_id VARCHAR(30) REFERENCES users(username)
+    status VARCHAR(64),
+    username VARCHAR(30) REFERENCES users(username)
 );
