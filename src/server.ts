d40dc041e30066d43d07248d6a98b50c71439835
abdulaziz <aabuainain@nic.gov.sa>
2022-11-19 01:40:35 +0300

    enabling cors as a middleware on all endpoints

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors'
 
 const app = express();
 const address = 'localhost';
 const port = 8000;
 
+app.use(cors())
 app.use(bodyParser.json());
 
 // routes TODO
@@ -12,6 +14,7 @@ app.use(bodyParser.json());
 // userRoutes(app);
 // orderRoutes(app);
 //
-app.listen(3000, function () {
+
+app.listen(port, function () {
   console.log(`starting app on: ${address}:${port}`);
 });
