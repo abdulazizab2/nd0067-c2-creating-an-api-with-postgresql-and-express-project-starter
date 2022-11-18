60f5fd8070cd449a9890ebcedbfc27d1e4673fb7
abdulaziz <aabuainain@nic.gov.sa>
2022-11-19 00:27:09 +0300

    minor refactor in server entry point and TODO comments

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import express, { Request, Response } from 'express'
-import bodyParser from 'body-parser'
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
 
-const app: express.Application = express()
-const address: string = "0.0.0.0:3000"
+const app: express.Application = express();
+const address = 'localhost';
+const port = 8000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', function (req: Request, res: Response) {
-    res.send('Hello World!')
-})
-
+// routes TODO
+// productRoutes(app);
+// userRoutes(app);
+// orderRoutes(app);
+//
 app.listen(3000, function () {
-    console.log(`starting app on: ${address}`)
-})
+  console.log(`starting app on: ${address}:${port}`);
+});
